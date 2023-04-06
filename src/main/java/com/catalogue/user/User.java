d87d682f8558a4edac4f29470c007f799e85f428
Okayreet <darkerror@hotmail.co.uk>
2023-04-06 17:42:04 +0100

    seperate login logics

--- src/main/java/com/catalogue/user/User.java
+++ src/main/java/com/catalogue/user/User.java
@@ -5,25 +5,30 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Entity(name = "users")
 public class User {
     @Id
     @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
     private Long id;
+    @NotBlank(message = "First Name cannot be blank")
     private String firstName;
+    @NotBlank(message = "Last Name cannot be blank")
     private String lastName;
+    @NotBlank(message = "Address 1 cannot be blank")
     private String address1;
+    @NotBlank(message = "Address 2 cannot be blank")
     private String address2;
+    @NotBlank(message = "City cannot be blank")
     private String city;
+    @NotBlank(message = "Postcode cannot be blank")
     private String postcode;
+    @NotBlank(message = "phoneNumber cannot be blank")
+    @Pattern(regexp = "^\\d{9,15}$", message = "Digit only")
+    private String phoneNumber;
 }
